ee0204febba22fcd3b91612b4405c6fd6a759e49
leeseean chiu <leeseean@qq.com>
2017-07-11 23:16:14 +0800

    改成面向对象方式，改名

--- js/race.js
+++ js/race.js
@@ -1,22 +1,29 @@
 let canvas = document.querySelector('#canvas');
 let context = canvas.getContext('2d');
 
-let i = 0;
-let bgTimeout = null;
-function bgTrack() {
-    let img = new Image();
-    img.width = canvas.width;
-    img.height = canvas.height;
-    img.onload = function () {
-        context.drawImage(this,0,0,img.width,img.height);
-    };
-
-    img.src = `./images/bg_track/bg_track_${i}.jpg`;
-    i++;
-    if(i>396){
-        clearTimeout(bgTimeout);
-        return;
+class BgTrack {
+    constructor(ctx,count,FPS){
+        this.ctx = ctx;
+        this.count = count;
+        this.FPS = FPS;
+        this.timeout = null;
+        this.i = 0;
     }
-    bgTimeout = setTimeout(bgTrack,100);
+    move(){
+        let img = new Image();
+        this.ctx.clearRect(0,0,img.width,img.height);
+        img.width = canvas.width;
+        img.height = canvas.height;
+        img.onload = () => this.ctx.drawImage(img,0,0,img.width,img.height);
+        img.src = `./images/bg_track/bg_track_${this.i}.jpg`;
+        this.i++;
+        if(this.i>this.count){
+            clearTimeout(this.timeout);
+            return;
+        }
+        this.timeout = setTimeout(this.move.bind(this),this.FPS);
+    }
 }
-bgTrack();
+
+let bgTrack = new BgTrack(context,396,100);
+bgTrack.move();
