a4863b88191a8bbb3dcff8d34177b35df50e7421
leeseean chiu <leeseean@qq.com>
2017-07-11 23:56:00 +0800

    文件改名

--- dist/js/race.js
+++ dist/js/race.js
@@ -8,14 +8,15 @@ var canvas = document.querySelector('#canvas');
 var context = canvas.getContext('2d');
 
 var BgTrack = function () {
+    //背景类
     function BgTrack(ctx, count, FPS) {
         _classCallCheck(this, BgTrack);
 
-        this.ctx = ctx;
-        this.count = count;
-        this.FPS = FPS;
-        this.timeout = null;
-        this.i = 0;
+        this.ctx = ctx; //画笔
+        this.count = count; //背景图片张数
+        this.FPS = FPS; //更新频率
+        this.timeout = null; //定时器
+        this.i = 0; //累加，每个序号对应不同图片
     }
 
     _createClass(BgTrack, [{
@@ -24,19 +25,20 @@ var BgTrack = function () {
             var _this = this;
 
             var img = new Image();
-            this.ctx.clearRect(0, 0, img.width, img.height);
+            this.ctx.clearRect(0, 0, img.width, img.height); //清除上一张图片释放内存
             img.width = canvas.width;
             img.height = canvas.height;
             img.onload = function () {
                 return _this.ctx.drawImage(img, 0, 0, img.width, img.height);
-            };
+            }; //画入背景图片
             img.src = './images/bg_track/bg_track_' + this.i + '.jpg';
             this.i++;
             if (this.i > this.count) {
+                //图片个数超过背景图总数后停止画入图片
                 clearTimeout(this.timeout);
                 return;
             }
-            this.timeout = setTimeout(this.move.bind(this), this.FPS);
+            this.timeout = setTimeout(this.move.bind(this), this.FPS); //不断画入图片
         }
     }]);
 
@@ -44,5 +46,22 @@ var BgTrack = function () {
 }();
 
 var bgTrack = new BgTrack(context, 396, 100);
-bgTrack.move();
+// bgTrack.move();
+
+var Horse = function () {
+    //马类
+    function Horse() {
+        _classCallCheck(this, Horse);
+    }
+
+    _createClass(Horse, [{
+        key: 'run',
+        value: function run() {}
+    }, {
+        key: 'forward',
+        value: function forward() {}
+    }]);
+
+    return Horse;
+}();
 //# sourceMappingURL=race.js.map
--- js/race.js
+++ js/race.js
@@ -1,29 +1,41 @@
 let canvas = document.querySelector('#canvas');
 let context = canvas.getContext('2d');
 
-class BgTrack {
+class BgTrack {//背景类
     constructor(ctx,count,FPS){
-        this.ctx = ctx;
-        this.count = count;
-        this.FPS = FPS;
-        this.timeout = null;
-        this.i = 0;
+        this.ctx = ctx;//画笔
+        this.count = count;//背景图片张数
+        this.FPS = FPS;//更新频率
+        this.timeout = null;//定时器
+        this.i = 0;//累加，每个序号对应不同图片
     }
     move(){
         let img = new Image();
-        this.ctx.clearRect(0,0,img.width,img.height);
+        this.ctx.clearRect(0,0,img.width,img.height);//清除上一张图片释放内存
         img.width = canvas.width;
         img.height = canvas.height;
-        img.onload = () => this.ctx.drawImage(img,0,0,img.width,img.height);
+        img.onload = () => this.ctx.drawImage(img,0,0,img.width,img.height);//画入背景图片
         img.src = `./images/bg_track/bg_track_${this.i}.jpg`;
         this.i++;
-        if(this.i>this.count){
+        if(this.i>this.count){//图片个数超过背景图总数后停止画入图片
             clearTimeout(this.timeout);
             return;
         }
-        this.timeout = setTimeout(this.move.bind(this),this.FPS);
+        this.timeout = setTimeout(this.move.bind(this),this.FPS);//不断画入图片
     }
 }
 
 let bgTrack = new BgTrack(context,396,100);
-bgTrack.move();
+// bgTrack.move();
+
+class Horse {//马类
+    constructor(){
+
+    }
+    run(){
+
+    }
+    forward(){
+        
+    }
+}
